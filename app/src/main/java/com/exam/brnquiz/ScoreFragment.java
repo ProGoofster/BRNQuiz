aa623dd99e89f9064e7ad02d540346ce5c0185a1
Jake Berryman <59934842+ProGoofster@users.noreply.github.com>
2025-10-30 12:17:49 -0500

    finish for real this time

--- app/src/main/java/com/exam/brnquiz/ScoreFragment.java
+++ app/src/main/java/com/exam/brnquiz/ScoreFragment.java
@@ -2,20 +2,31 @@ package com.exam.brnquiz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ScoreFragment extends Fragment {
 
+    private BRNViewModel brnViewModel;
+    private TextView tv;
+
+    // This field is no longer necessary as the data is handled by the ViewModel
+    // String scoreText;
+
     public ScoreFragment() {
         // Required empty public constructor
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +37,23 @@ public class ScoreFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_score, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tv = view.findViewById(R.id.score_number);
+
+        brnViewModel = new ViewModelProvider(requireActivity()).get(BRNViewModel.class);
+
+        brnViewModel.getPoints().observe(getViewLifecycleOwner(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer points) {
+                if (points != null) {
+                    tv.setText(String.valueOf(points));
+                }
+            }
+        });
     }
 }
