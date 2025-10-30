08a63993db4d6d9688c0bb2b95d9d3e9ecd226e1
Jake Berryman <59934842+ProGoofster@users.noreply.github.com>
2025-10-30 12:10:47 -0500

    finish

--- app/src/main/java/com/exam/brnquiz/IntroActivity.java
+++ app/src/main/java/com/exam/brnquiz/IntroActivity.java
@@ -1,11 +1,15 @@
 package com.exam.brnquiz;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +23,11 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECORD_AUDIO }, 67);
+        }
 
 
 
